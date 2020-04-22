8963d6289e7cffe950b84f0abf7d5c6f90a7de90
Yash Akotkar <>
2020-04-22 18:55:03 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,47 @@
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const user = require("./routes/index");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/", user);
+
+mongoose.connect("mongodb://localhost:27017/medicoDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+const db = mongoose.connection;
+db.on("error", function(err) { console.error(err) });
+db.once("open", function() { console.log("Connected to Mongoose") });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Listen
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server running on poet 3000");
+});
